b50cfbec98bfcbc308196c9a35b5879f2a8026b0
LuthfiKurnia29 <lkurniahadi@gmail.com>
2023-06-25 22:42:20 +0700

    mendeploy website ke firebase server

--- script.js
+++
@@ -1,7 +0,0 @@
-// let a = 8;
-// let b = 10;
-// console.log(a + b);
-
-const navlink = document.querySelectorAll('.nav-link').forEach(e => e.addEventListener('click',function() {
-    e.classList.toggle('active');
-}))
